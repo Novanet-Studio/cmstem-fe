f6f83799695544658918c1f9b4ae64785ae882e7
David Arenas <davejs136@gmail.com>
2023-08-10 11:45:29 -0400

    fix: product-image no render correctly

--- components/product/thumbnail-image.vue
+++ components/product/thumbnail-image.vue
@@ -8,12 +8,19 @@ interface Props {
 defineProps<Props>();
 
 const product = inject(injectKeys.product) as Product;
-const imageUrl = computed(() => product?.images[0].formats?.small?.url);
+const imageUrl = computed(() => product?.images[0].formats.thumbnail.url);
 </script>
 
 <template>
   <nuxt-link :class="class" :to="`/product/${product.id}`">
-    <nuxt-img v-if="imageUrl" :src="imageUrl" :alt="product.name" :placeholder="[100, 50, 10]"
-      sizes="sm:100vw md:50vw lg:200px" fit="outside" class="h-full w-full object-contain" />
+    <nuxt-img
+      v-if="imageUrl"
+      :src="imageUrl"
+      :alt="product.name"
+      :placeholder="[100, 50, 10]"
+      sizes="sm:100vw md:50vw lg:200px"
+      fit="outside"
+      class="h-full w-full object-contain"
+    />
   </nuxt-link>
 </template>
