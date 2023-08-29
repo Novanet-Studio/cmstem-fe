ccaa9a07d5879a3ffdbadb2dd2d2abb64e3d080e
David Arenas <davejs136@gmail.com>
2023-08-29 19:09:13 -0400

    style: add rounded borders to tables

--- components/table/wrapper.vue
+++ components/table/wrapper.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col">
     <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
       <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="overflow-hidden rounded-md">
+        <div class="overflow-hidden rounded-3xl">
           <slot />
         </div>
       </div>
