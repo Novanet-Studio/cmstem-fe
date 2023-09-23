0733405a1ce1892c02956fa91ba6b153bef37a79
David Arenas <davejs136@gmail.com>
2023-09-23 18:20:07 -0400

    refactor: change `Agregar al carrito` for btn icon, add overlay with `no stock`

--- components/product/default.vue
+++ components/product/default.vue
@@ -59,15 +59,22 @@ provide(injectKeys.product, props.product);
 <template>
   <div class="lg:(p-[20px_20px_0] pb-8) flex flex-col items-center">
     <div class="product relative p-3 lg:p-0">
-      <div class="product__thumbnail lg:pb-4">
+      <div class="product__thumbnail lg:pb-4 relative">
         <product-thumbnail-image />
         <!-- <product-actions @quick-view="handleQuickView" /> -->
+        <div
+          class="absolute bg-dark/70 w-full h-full top-0 left-0 flex justify-center items-center"
+          v-if="!product.size_stock?.length"
+        >
+          <span class="text-2xl font-bold text-white">Sin stock</span>
+        </div>
       </div>
       <div
         class="!absolute -bottom-5 lg:-bottom-8 left-0 w-full flex justify-center"
       >
         <product-price
           class="!my-1 w-40px h-40px text-xs lg:(w-50px h-50px text-base) bg-color-3 flex justify-center items-center rounded-full !text-white font-bold"
+          v-if="product.size_stock?.length"
           >{{ product.price }}</product-price
         >
       </div>
@@ -75,13 +82,14 @@ provide(injectKeys.product, props.product);
     <div class="mt-6 lg:mt-8">
       <product-title :id="product.id">{{ product.name }}</product-title>
     </div>
-    <div class="w-full mt-2 px-4 mx-12 pb-2">
+    <div class="mt-2 px-4 mx-12 pb-2">
       <button
-        class="py-2 px-8 rounded-full w-full bg-color-2 shadow-md shadow-black/20 text-color-5 font-bold text-xs lg:text-base disabled:opacity-50"
+        class="py-2 px-6 rounded-full w-full shadow-md shadow-black/20 text-color-5 font-bold text-xs lg:text-base disabled:opacity-50"
         :disabled="!product.size_stock?.length"
         @click="handleAddToCart"
       >
-        {{ product.size_stock?.length ? 'Agregar al carrito' : 'Agotado' }}
+        <!-- {{ product.size_stock?.length ? 'Agregar al carrito' : 'Agotado' }} -->
+        <div class="i-ph-shopping-bag text-2xl text-color-1"></div>
       </button>
     </div>
   </div>
@@ -89,7 +97,7 @@ provide(injectKeys.product, props.product);
 
 <style scoped>
 .product {
-  @apply h-full box-border relative block box-border border rounded-xl transition ease hover:(border border-gray-300) w-[168px] max-w-[168px] lg:(w-250px max-w-[250px] p-[20px_20px_0]);
+  @apply h-full box-border relative block box-border border rounded-xl transition ease hover:(border border-gray-300) w-[168px] max-w-[168px] lg:(w-250px max-w-[250px] p-[20px_20px_20px]);
 }
 
 .product__thumbnail {
