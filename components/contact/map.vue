6233b2fe9d992ae2ff80649211cc1d62e8948325
Fredy Polania <fpolania@gmail.com>
2023-09-29 20:36:50 -0400

    add: contact map current src

--- components/contact/map.vue
+++ components/contact/map.vue
@@ -5,8 +5,8 @@
     </h3>
     <div class="w-full p-4 border rounded-2xl lg:p-4">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44387.332515724636!2d-66.84548302724721!3d10.475185155826471!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2a58adcd824807%3A0x93dd2eae0a998483!2sCaracas%2C%20Capital%20District!5e0!3m2!1sen!2sve!4v1652817598082!5m2!1sen!2sve"
-        class="w-full rounded-xl lg:h-720px"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3922.8733196320404!2d-66.8745303!3d10.5106427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2a59116aaaaaab%3A0x75977b48cd36263e!2sCaracas%20MultiSport!5e0!3m2!1sen!2sve!4v1696033495738!5m2!1sen!2sve"
+        class="w-full rounded-xl lg:h-650px"
         height="535"
         style="border: 0"
         allowfullscreen
