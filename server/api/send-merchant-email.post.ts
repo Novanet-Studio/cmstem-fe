faee48581de0ba57b8ac96c37a5e8193bd79b0e6
David Arenas <davejs136@gmail.com>
2023-10-16 14:38:22 -0400

    fix: comment email functionality

--- server/api/send-merchant-email.post.ts
+++ server/api/send-merchant-email.post.ts
@@ -1,30 +1,30 @@
-import { useCompiler } from '#vue-email';
-import sgMail from '@sendgrid/mail';
+// import { useCompiler } from '#vue-email';
+// import sgMail from '@sendgrid/mail';
 
 export default defineEventHandler(async (event) => {
   try {
-    const data = await readBody(event);
-    const { sendgrid } = useRuntimeConfig();
+    // const data = await readBody(event);
+    // const { sendgrid } = useRuntimeConfig();
 
-    const template = await useCompiler('receipt.vue', {
-      props: {
-        body: {
-          ...data,
-        },
-      },
-    });
+    // const template = await useCompiler('receipt.vue', {
+    //   props: {
+    //     body: {
+    //       ...data,
+    //     },
+    //   },
+    // });
 
-    sgMail.setApiKey(sendgrid.apiKey);
+    // sgMail.setApiKey(sendgrid.apiKey);
 
-    const payload = {
-      to: sendgrid.receiverEmail,
-      from: sendgrid.senderEmail,
-      subject: `CCS MultiSport nueva orden ${data.orderId}`,
-      text: 'CCS MultiSport nueva orden',
-      html: template,
-    };
+    // const payload = {
+    //   to: sendgrid.receiverEmail,
+    //   from: sendgrid.senderEmail,
+    //   subject: `CCS MultiSport nueva orden ${data.orderId}`,
+    //   text: 'CCS MultiSport nueva orden',
+    //   html: template,
+    // };
 
-    await sgMail.send(payload);
+    // await sgMail.send(payload);
 
     return {
       message: 'Email enviado',
--- server/api/send-receipt-email.post.ts
+++ server/api/send-receipt-email.post.ts
@@ -1,30 +1,30 @@
-import { useCompiler } from '#vue-email';
-import sgMail from '@sendgrid/mail';
+// import { useCompiler } from '#vue-email';
+// import sgMail from '@sendgrid/mail';
 
 export default defineEventHandler(async (event) => {
   try {
-    const data = await readBody(event);
-    const { sendgrid } = useRuntimeConfig();
+    // const data = await readBody(event);
+    // const { sendgrid } = useRuntimeConfig();
 
-    const template = await useCompiler('receipt.vue', {
-      props: {
-        body: {
-          ...data,
-        },
-      },
-    });
+    // const template = await useCompiler('receipt.vue', {
+    //   props: {
+    //     body: {
+    //       ...data,
+    //     },
+    //   },
+    // });
 
-    sgMail.setApiKey(sendgrid.apiKey);
+    // sgMail.setApiKey(sendgrid.apiKey);
 
-    const payload = {
-      to: data.email,
-      from: sendgrid.senderEmail,
-      subject: `CCS MultiSport recibo de compra ${data.orderId}`,
-      text: 'CCS MultiSport recibo de compra',
-      html: template,
-    };
+    // const payload = {
+    //   to: data.email,
+    //   from: sendgrid.senderEmail,
+    //   subject: `CCS MultiSport recibo de compra ${data.orderId}`,
+    //   text: 'CCS MultiSport recibo de compra',
+    //   html: template,
+    // };
 
-    await sgMail.send(payload);
+    // await sgMail.send(payload);
 
     return {
       message: 'Email enviado',
